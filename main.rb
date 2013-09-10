2880a1ce4dfb5ffb987d76dc24010b3b36eb4a8a
kgtong <kgtong1992@gmail.com>
2013-09-10 10:26:48 +0800

    增加了反馈数据为空处理

--- main.rb
+++ main.rb
@@ -2,18 +2,22 @@
 require 'json'
 load 'easy_physics.rb'
 
-sid = '1120310618'
-password = '1120310618'
+sid      = '1120310617'
+password = '1120310617'
 
 #format as: 
-#data = { 'name' => 'xxx', 
-#         'test' => [
-#                    {name, add, seat, week, date, period},
-#                    {name, add, seat, week, date, period},
-#                    {name, add, seat, week, date, period},
-#                    {name, add, seat, week, date, period}
-#                  ]
-#        }
+#       data = { 'name' => 'xxx', 
+#                 'test' => [
+#                             {name, add, seat, week, date, period},
+#                             {name, add, seat, week, date, period},
+#                             {name, add, seat, week, date, period},
+#                             {name, add, seat, week, date, period}
+#                           ]
+#               }
+#when sid or password is illegal
+#       data = '用户名或密码不正确'
+#when fetching for the booking is nil
+#       data = '尚无预约'
 
 #to-do
 #get the score of the user
@@ -23,19 +27,30 @@ password = '1120310618'
 physics = EasyPhysics.new(sid, password)
 
 cookies = physics.post_and_get_cookies
+if cookies.empty?
+  physics.data = '用户名或密码不正确'
+else
+  html = physics.request_page_and_get_html(cookies)
 
-html = physics.request_page_and_get_html(cookies)
+  #get the array of match
+  es = physics.match_html(html)
 
-#get the array of match
-es = physics.match_html(html)
+  #尚无预约
+  if es.empty?
+    physics.data = '尚无预约'
+  else
+    name = physics.get_name(es)
 
-name = physics.get_name(es)
+    arrs = physics.solve_matching_info(es)
 
-arrs = physics.solve_matching_info(es)
+    #get the all the experiments encapsulated with the form of hash
+    test = physics.format_data(arrs)
 
-#get the all the experiments encapsulated with the form of hash
-test = physics.format_data(arrs)
+    physics.set_data(name, test)
+  
+  end
+end
+  
+f=File.new(File.join("./tmp","#{sid}.txt"), "w+")
+f.puts physics.data.to_json
 
-physics.set_data(name, test)
-
-puts physics.data.to_json
